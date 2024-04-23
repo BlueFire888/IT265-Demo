922a8220eac21e02748028f83a7acc2a1f92f4a8
MattToegel <matthew.toegel@njit.edu>
2024-04-22 20:51:11 -0400

    modded selector and setup tools to let you place items before a game, allow basic turns and movement of pieces

--- IT265Tools/Assets/ObjectSelector.cs
+++ IT265Tools/Assets/ObjectSelector.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ObjectSelector : MonoBehaviour
 {
@@ -9,8 +10,12 @@ public class ObjectSelector : MonoBehaviour
     [SerializeField] private GameObject selected;
     [SerializeField] private bool fixOffset = false;
     [SerializeField] private LayerMask layersToInteractWith;
+    [SerializeField] private LayerMask placementLayers;
     [SerializeField]
     private Vector3 specificOffset;
+    [SerializeField] private PiecesInventory piecesInventoryRef;
+    [SerializeField] public bool rightClickToDelete = false;
+    public bool allowMove = false;
     private void Awake()
     {
         var c = GetComponent<Camera>();
@@ -23,8 +28,29 @@ public class ObjectSelector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (rightClickToDelete && Input.GetMouseButtonDown(1))
+        {
+            Vector3 mouse = Vector3.zero;
+            mouse.x = Input.mousePosition.x;
+            mouse.y = Input.mousePosition.y;
+            Ray ray = cam.ScreenPointToRay(mouse);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                int layer = hitInfo.collider.gameObject.layer;
+                if ((layersToInteractWith & (1 << layer)) != 0)
+                {
+                    //layer exists in mask
+                    Destroy(hitInfo.collider.gameObject);
+                }
+            }
+        }
         if (Input.GetMouseButtonDown(0))
         {
+            if (!allowMove && piecesInventoryRef != null)
+            {
+                selected = piecesInventoryRef.GetCurrentObject();
+            }
             Debug.Log("User clicked the left mouse button");
             Vector3 mouse = Vector3.zero;
             mouse.x = Input.mousePosition.x;
@@ -45,14 +71,15 @@ public class ObjectSelector : MonoBehaviour
                 {
                     Debug.Log("Placed item");
                     int layer = hitInfo.collider.gameObject.layer;
-                    if ((layersToInteractWith & (1 << layer)) != 0)
+                    if ((placementLayers & (1 << layer)) != 0)
                     {
                         //layer exists in mask
-                        return;// don't place items if the object is in the mask
+                       
                     }
                     else
                     {
                         //layer doesn't exist in mask
+                        return;// don't place items if the object is not in the placement layers mask
                     }
                     if (fixOffset)
                     {
@@ -64,12 +91,27 @@ public class ObjectSelector : MonoBehaviour
                             //this item is a tile
                             pos = tile.GetPointPosition();
                         }
-                        selected.transform.position = pos + specificOffset; //pos + offset;
-                        
+
+                        if (!allowMove && piecesInventoryRef != null)
+                        {
+                            Instantiate(selected, pos + specificOffset, Quaternion.identity);
+                        }
+                        else if(selected != null)
+                        {
+                            selected.transform.position = pos + specificOffset; //pos + offset;
+                        }
+
                     }
                     else
                     {
-                        selected.transform.position = hitInfo.point;
+                        if (!allowMove && piecesInventoryRef != null)
+                        {
+                            Instantiate(selected, hitInfo.point, Quaternion.identity);
+                        }
+                        else if(selected != null)
+                        {
+                            selected.transform.position = hitInfo.point;
+                        }
                     }
                    
                 }
@@ -81,6 +123,11 @@ public class ObjectSelector : MonoBehaviour
                     {
                         //layer exists in mask
                         selected = hitInfo.collider.gameObject;
+                        Selectable s = selected.GetComponent<Selectable>();
+                        if (s != null)
+                        {
+                            s.SetSelected(true);
+                        }
                     }
                     else
                     {
@@ -91,6 +138,14 @@ public class ObjectSelector : MonoBehaviour
             }
             else
             {
+                if (selected != null)
+                {
+                    Selectable s = selected.GetComponent<Selectable>();
+                    if (s != null)
+                    {
+                        s.SetSelected(false);
+                    }
+                }
                 selected = null;
             }
         }
