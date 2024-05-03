8e00c17add5649e87bc15fa5b868a82730229f56
Michael Jones <mjj4@njit.edu>
2024-05-03 16:41:41 -0400

    trying to switch branches

---
+++ IT265Tools/Assets/_IT265/Scripts/BasicTile1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTile1 : MonoBehaviour
+{
+    [SerializeField] private GameObject point;
+
+    public Vector3 GetPointPosition()
+    {
+        return point.transform.position;
+    }
+}
